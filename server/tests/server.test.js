ccbb553ab6ee53617907740f196219e2ca8edebb
Shaun Chua <shon3005@gmail.com>
2017-06-11 12:05:20 -0400

    add delete tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -166,6 +166,8 @@ describe('GET /users/me', () => {
 
 describe('POST /users', () => {
 	it('should create a user', (done) => {
+		var email = 'example@example.com';
+	    var password = '123mnb!';
 		request(app).post('/users').send({email, password}).expect(200).expect((res) => {
 			expect(res.headers['x-auth']).toExist();
 			expect(res.body._id).toExist();
@@ -201,7 +203,7 @@ describe('POST /users/login', () => {
 	it('should login user and return auth token', (done) => {
 		request(app).post('/users/login').send({
 			email: users[1].email,
-			password: users[1].password + '1'
+			password: users[1].password
 		}).expect(200).expect((res) => {
 			expect(res.headers['x-auth']).toExist();
 		}).end((error, res)  => {
@@ -209,7 +211,7 @@ describe('POST /users/login', () => {
 				return done(error);
 			}
 			User.findById(users[1]._id).then((user) => {
-				expect(user.tokens[0]).toInclude){
+				expect(user.tokens[0]).toInclude({
 					access: 'auth',
 					token: res.headers['x-auth']
 				});
@@ -221,7 +223,7 @@ describe('POST /users/login', () => {
 	it('should reject invalid login', (done) => {
 		request(app).post('/users/login').send({
 			email: users[1].email,
-			password: users[1].password
+			password: users[1].password + '1'
 		}).expect(400).expect((res) => {
 			expect(res.headers['x-auth']).toNotExist();
 		}).end((error, res) => {
@@ -229,9 +231,29 @@ describe('POST /users/login', () => {
 				return done(error);
 			}
 			User.findById(users[1]._id).then((user) => {
-				expect(user.tokens[0]).toBe(0);
+				expect(user.tokens.length).toBe(0);
 				done();
 			}).catch((e) => done(e));
 		});
 	});
 });
+
+describe('DELETE /users/me/token', () => {
+	it('should remove auth token on logout', (done) => {
+		// DELETE /users/me/token
+		// set x-auth equal to token
+		// 200
+		// find user, verify that tokens array has length 0
+		request(app).delete('/users/me/token').set('x-auth', users[0].tokens[0].token).expect(200).end((err, res) => {
+			if (err) {
+				return done(error);
+			}
+
+			User.findById(users[0]._id).then((user) => {
+				expect(user.tokens.length).toBe(0);
+				done();
+			}).catch((e) => done(e));
+		})
+
+	})
+})
